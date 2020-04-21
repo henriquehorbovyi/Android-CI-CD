630b451bc0bea1e6fc44f138443487aa83d3675f
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2020-04-21 19:19:59 -0300

    updated

--- setup.sh
+++ setup.sh
@@ -3,11 +3,11 @@ set -eu
 
 chmod +x ./gradlew
 
-if [ -n "$1" ]; then
-    mkdir -p $ANDROID_HOME/licenses
-    echo -e $1 >> $ANDROID_HOME/licenses/android-sdk-license
-    echo $"\nLicences accepted!"
-fi
+# if [ -n "$1" ]; then
+#     mkdir -p $ANDROID_HOME/licenses
+#     echo -e $1 >> $ANDROID_HOME/licenses/android-sdk-license
+#     echo $"\nLicences accepted!"
+# fi
 
 # echo $"\n--> Running './gradlew $2'\n"
 # sh -c "./gradlew $2"
